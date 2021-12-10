a07704c47e359d70810668959b7a1589ce2a6d46
UIT.20521737 <85834282+UIT20521737@users.noreply.github.com>
2021-12-10 11:51:08 +0700

    Add new files
    Thêm các file hoàn chỉnh

---
+++ snakegame.h
@@ -0,0 +1,44 @@
+#ifndef SNAKEGAME_H
+#define SNAKEGAME_H
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <conio.h>
+#include <string>
+#include <stdlib.h>    
+#include <time.h>
+#include <iomanip>
+using namespace std;
+struct SNAKE {
+	vector<int>x;
+	vector<int>y;
+	int length;
+};
+struct FOOD {
+	int x;
+	int y;
+};
+int random(int, int);
+void setcursor(bool, DWORD);
+void gotoxy(int, int);
+void setcolor(unsigned short);
+void createSnake(SNAKE&);
+void drawSnake(SNAKE);
+void drawGameWall();
+void removeOldSnake(SNAKE);
+void setSnake(SNAKE&, int, int);
+bool checkTouchWall(int, int);
+bool checkEatSnakeTail(SNAKE, int, int);
+void createAndDrawFood(FOOD&);
+void drawScore();
+void snakeGame();
+void menu();
+void drawRightButton(int, int);
+void drawLeftButton(int, int);
+void drawStarButton(int, int);
+void drawQuitButton(int, int);
+void removeButton(int, int);
+void removeStartButton(int, int);
+void removeQuitButton(int, int);
+
+#endif // !SNAKEGAME_H
